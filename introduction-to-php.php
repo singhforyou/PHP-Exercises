ec8ea01213fe7f0cb294ee091d563ad7e53c5753
Simranjit Singh <sing1182@algonquinlive.com>
2019-01-08 11:09:31 -0500

    added introduction to PHP

---
+++ introduction-to-php.php
@@ -0,0 +1,48 @@
+<?php
+
+echo "Welcome to PHP!";
+
+echo "<hr style=\"margin 1rem 0\">";
+
+echo "PHP stands for \"Hypertext Preprocessor\"!";
+
+echo "<hr style=\"margin 1rem 0\">";
+
+echo "<p>PHP stands for \"Hypertext Preprocessor\"!</p>";
+
+echo "<hr style=\"margin 1rem 0\">";
+
+echo "HTML - Stands for Hypertext Markup Language";
+
+echo "<br>";
+
+echo "CSS - Stands for Cascading Stylesheet";
+
+echo "<br>";
+
+echo "Stands for JavaScript";
+
+echo "<br>";
+
+echo "Stands for Hypertext Preprocessor";
+
+echo "<br>";
+
+echo "SQL - Stands for Structural Query Language";
+
+echo "<br>";
+
+echo "<hr style=\"margin 1rem 0\">";
+
+
+
+echo "<ul>
+ <li>HTML - Stands for Hypertext Markup Language</li>
+ <li>CSS - Stands for Cascading Stylesheet</li>
+ <li>Stands for JavaScript</li>
+ <li>Stands for Hypertext Preprocessor</li>
+ <li>SQL - Stands for Structural Query Language</li>
+</ul>";
+
+
+?>
